380cf41aac8d1f5b1c446247017c0b379d10d81f
root <you@example.com>
2013-10-26 09:45:22 -0300

    Se corrigieron accesos de backbone para no interferir en las rutas. Se agrego pagina de API. -lg

---
+++ coperable-site/routes/development.js
@@ -0,0 +1,15 @@
+var _ = require('underscore')
+
+exports.index = function(req, res) {
+
+  var geo = (req.session ? req.session.geo : false) ||
+            (req.cookies ? req.cookies.geo : undefined)
+
+  return res.render('development/index.html', {
+    layout: 'layout-dev',
+    layoutTitle: 'Desarrollo',
+    layoutId: 'dev-index',
+    user: req.user,
+    geo: geo
+  })
+};
